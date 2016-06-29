ceba47b5484d74236c6897069c515cf8cf4e5af7
“wuzhong” <wuzhong@tuniu.com>
2016-06-29 10:40:22 +0800

    增加下拉加载 和上拉刷新

--- community-help-console/src/main/webapp/js/common.js
+++ community-help-console/src/main/webapp/js/common.js
@@ -1,1 +1,1 @@
-$(function(){        //用于控制图片自适应宽度和高度        var autocss = {"max-width":"100%", "height":"auto"};        $(".InfoContent img,.ChannelContent img").css(autocss);});
+$(function(){        //用于控制图片自适应宽度和高度        var autocss = {"max-width":"100%", "height":"auto"};        $(".InfoContent scroll,.ChannelContent scroll").css(autocss);});
